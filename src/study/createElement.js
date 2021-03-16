65d586b0ee22cc9c6ab52a9b31e5af92a2bb70ec
GreatGhost <1609401009@qq.com>
2021-03-16 08:57:33 +0800

    手写递归创建子节点

--- src/study/createElement.js
+++ src/study/createElement.js
@@ -9,8 +9,17 @@ export default function(vnode,pivot){
         // 孤儿节点上树
         domNode.innerText=vnode.text;
         // pivot.parentNode.insertBefore(domNode,pivot)
-        vnode.ele=domNode;
+    }else if(Array.isArray(vnode.children) && vnode.children.length>0){
+        //存在内部子节点，就要递归
+        for(let i=0;i<vnode.children.length;i++){
+            let ch=vnode.children[i];
+            let chDOM=createElement(ch);
+            domNode.appendChild(chDOM);
+        }
     }
+
+    // 补充ele属性
+    vnode.ele=domNode;
     return vnode.ele;
     
 }
